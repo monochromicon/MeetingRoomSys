c5a9900eeb5a8aea389146e1be10524c90572229
Graham Barber <grahammbarber@gmail.com>
2015-06-30 13:43:16 -0700

    Created gulpfile, build command

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require("gulp"),
+	minHTML = require("gulp-minify-html"),
+	minCSS = require("gulp-minify-css"),
+	jade = require("gulp-jade"),
+	less = require("gulp-less"),
+	uglify = require("gulp-uglify"),
+	connect = require("gulp-connect");
+	
+gulp.task("build", function() {
+	gulp.src("src/public/index.jade")
+		.pipe(jade())
+		.pipe(minHTML())
+		.pipe(gulp.dest("dist/public/"));
+		
+	gulp.src("src/server.js")
+		.pipe(uglify({ mangle: false }))
+		.pipe(gulp.dest("dist/"))
+		
+	gulp.src("src/public/js/**.js")
+		.pipe(uglify({ mangle: false }))
+		.pipe(gulp.dest("dist/public/js/"))
+		
+	gulp.src("src/public/less/**.less")
+		.pipe(less())
+		.pipe(minCSS())
+		.pipe(gulp.dest("dist/public/css/"))
+});
