12d44c3ffbef1fe74703e9451b2dd1aeaebce21b
Graham Barber <grahammbarber@gmail.com>
2015-08-12 16:33:04 -0700

    Removed Jade and LESS files, trimed package

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,10 @@
 var gulp = require("gulp"),
 	minHTML = require("gulp-minify-html"),
 	minCSS = require("gulp-minify-css"),
-	jade = require("gulp-jade"),
-	less = require("gulp-less"),
-	uglify = require("gulp-uglify"),
-	connect = require("gulp-connect");
+	uglify = require("gulp-uglify");
 	
 gulp.task("build", function() {
-	gulp.src("src/public/index.jade")
-		.pipe(jade())
+	gulp.src("src/public/index.html")
 		.pipe(minHTML())
 		.pipe(gulp.dest("dist/public/"));
 		
@@ -20,26 +16,13 @@ gulp.task("build", function() {
 		.pipe(uglify({ mangle: false }))
 		.pipe(gulp.dest("dist/public/js/"));
 		
-	gulp.src("src/public/less/**.less")
-		.pipe(less())
+	gulp.src("src/public/css/**.css")
 		.pipe(minCSS())
 		.pipe(gulp.dest("dist/public/css/"));
 });
 
-gulp.task("connect", function () {
-	connect.server({
-		port: 80,
-		root: "dist/public",
-		livereload: true
-	});
-});
-
-gulp.task("reload", function () {
-	connect.reload();
-});
-
-gulp.task("default", ["build", "connect"], function () {
-	gulp.watch("src/public/index.jade", ["build", "reload"]);
-	gulp.watch("src/public/js/**.js", ["build", "reload"]);
-	gulp.watch("src/public/less/**.less", ["build", "reload"]);
+gulp.task("default", ["build", "launch", "open"], function () {
+	gulp.watch("src/public/index.html", ["build"]);
+	gulp.watch("src/public/js/**.js", ["build"]);
+	gulp.watch("src/public/less/**.css", ["build"]);
 });
